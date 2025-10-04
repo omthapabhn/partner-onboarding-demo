cfc1835b344d23aac4d294ad338c7e382ecb252e
omthapabhn <om.thapa@bhn.com>
2025-10-04 14:05:43 +0530

    first cut of partner onboarding

---
+++ run_workflow.py
@@ -0,0 +1,20 @@
+import asyncio
+import config
+from run_worker import SayHello
+from temporalio.client import Client
+
+
+async def main():
+    # Create client connected to server at the given address
+    client = await Client.connect("localhost:7233")
+
+    # Execute a workflow
+    result = await client.execute_workflow(
+        SayHello.run, "Temporal", id="hello-workflow", task_queue=config.TASK_QUEUE_NAME
+    )
+
+    print(f"Result: {result}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
